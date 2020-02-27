70545766ed78ccfaed44f63700eb398a13e7189d
Alex Wild <alex.wild@epitech.eu>
2020-02-27 13:14:09 +0100

    modification of rotations, making it cleaner

--- src/enemy/enemy_movements.c
+++ src/enemy/enemy_movements.c
@@ -20,38 +20,16 @@ int next_status_diff(enemy_t *ene, path_t *next, path_t *current)
     return 0;
 }
 
-void do_rotation(enemy_t *ene, int status, int next_stat, path_t *current)
-{
-    float angle = sfSprite_getRotation(ene->sprite);
-
-        if (angle == 0 && status == 0 && next_stat == 2)
-            sfSprite_setRotation(ene->sprite, 90);
-        if (angle == 0 && status == 0 && next_stat == 3)
-            sfSprite_setRotation(ene->sprite, -90);
-        if (angle == 90 && status == 2 && next_stat == 0)
-            sfSprite_setRotation(ene->sprite, 0);
-        if (angle == 90 && status == 2 && next_stat == 1)
-            sfSprite_setRotation(ene->sprite, 180);
-        if (angle == 180 && status == 1 && next_stat == 2)
-            sfSprite_setRotation(ene->sprite, -90);
-        if (angle == 180 && status == 1 && next_stat == 3)
-            sfSprite_setRotation(ene->sprite, 270);
-        if (angle == 270 && status == 3 && next_stat == 0)
-            sfSprite_setRotation(ene->sprite, 0);
-        if (angle == 270 && status == 3 && next_stat == 1)
-            sfSprite_setRotation(ene->sprite, 180);
-}
-
 void do_y_move(enemy_t *ene, path_t *next, path_t *current)
 {
     if (next->pos.x == current->pos.x && next->pos.y > current->pos.y) {
         ene->pos.y += 3;
-        do_rotation(ene, ene->status, 2, current);
+        sfSprite_setRotation(ene->sprite, 90);
         ene->status = 2;
     }
     else if (next->pos.x == current->pos.x && next->pos.y < current->pos.y) {
         ene->pos.y -= 3;
-        do_rotation(ene, ene->status, 3, current);
+        sfSprite_setRotation(ene->sprite, -90);
         ene->status = 3;
     }
 }
@@ -60,13 +38,13 @@ void change_status(enemy_t *ene, path_t *current, path_t *next)
 {
     if (next->pos.y == current->pos.y && next->pos.x > current->pos.x) {
         ene->pos.x += 3;
-        do_rotation(ene, ene->status, 0, current);
+        sfSprite_setRotation(ene->sprite, 0);
         ene->status = 0;
     }
     else if (next->pos.y == current->pos.y && 
             next->pos.x < current->pos.x) {
         ene->pos.x -= 3;
-        do_rotation(ene, ene->status, 1, current);
+        sfSprite_setRotation(ene->sprite, 270);
         ene->status = 1;
     }
     else
